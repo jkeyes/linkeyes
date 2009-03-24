adab21372693641df29d37208fa54cfe8f0ee100
John Keyes <johnkeyes@gmail.com>
2009-03-24 16:36:00 +0000

    Pass args from shell script to python interpreter.

--- bin/linkeyes.sh
+++ bin/linkeyes.sh
@@ -1,3 +1,3 @@
 PATH=/usr/local/bin/:$PATH
 cd "`dirname $0`"
-PYTHONPATH=site-packages python2.5 site-packages/linkeyes/main.py 
+PYTHONPATH=../site-packages python2.5 ../site-packages/linkeyes/main.py $*
